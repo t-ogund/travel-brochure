40468dedfa502cb8c7d36434e69b4cc08512d55f
Toye Ogundepo <ogundepo@gmail.com>
2021-03-29 15:41:33 -0500

    style update.

--- script.js
+++ script.js
@@ -4,11 +4,24 @@ const hamburger = document.querySelector("#hamburger");
 const dropdown = document.querySelector("#dropdown-container");
 const dropdownNavContainer = document.querySelector("#dropdown-nav-container");
 
+const attractions = document.querySelector("#attractions");
+const latestPosts = document.querySelector("#latest-posts");
+
+// attractions.scrollIntoView({
+//   behavior: 'smooth', // smooth scroll
+//   block: 'start' // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
+// })
+
+// latestPosts.scrollIntoView({
+//   behavior: 'smooth', // smooth scroll
+//   block: 'start' // the upper border of the element will be aligned at the top of the visible part of the window of the scrollable area.
+// })
+
+
 hamburger.addEventListener("click", () => {
   dropdown.classList.toggle("hide");
   dropdown.style.textAlign = "center";
   dropdownNavContainer.addEventListener("click", (e) => {
-    console.log("IT WORKS!", e);
     dropdown.classList.add("hide");
   });
 });
